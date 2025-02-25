5faea5d80446d0149e336628d1d3c6b8be3f7aeb
maryamMH100 <mar0iam100@gmail.com>
2025-02-25 12:11:51 +0300

    Initial Commit

---
+++ PostureDetect/PostureDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureDetectApp.swift
+//  PostureDetect
+//
+//  Created by Maryam on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PostureDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
