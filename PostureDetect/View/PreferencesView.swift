f446a3bd365473f26a9a22f7a5fa64f7fd6c16e8
REEMAALHUDAIAN <reemaalhudaian@gmail.com>
2025-03-13 14:55:29 +0300

    NEW1

--- PostureDetect/View/PreferencesView.swift
+++ PostureDetect/View/PreferencesView.swift
@@ -32,7 +32,7 @@ struct PreferencesView: View {
     let notificationOptions = ["Once", "Twice", "Four times"]
 
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ZStack {
                 if isOnboarding {
                     Color.black.opacity(0.3)
